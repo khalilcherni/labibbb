bf5bec02d622824b452febcc9a700fc637b09bde
khalilcherni <kcherni411@gmail.com>
2024-02-12 16:36:18 +0100

    5

--- Backend/firebase/config.js
+++ Backend/firebase/config.js
@@ -5,9 +5,9 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API-KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId:  process.env.  NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: "labibb-a1ecc.appspot.com",
-  messagingSenderId: "188901640312",
+  projectId:  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:188901640312:web:076aa7bb995eee513f458d",
 //   measurementId: "G-W2TSHK4HL2"
 };
