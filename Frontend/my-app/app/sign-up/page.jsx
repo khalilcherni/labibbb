db2d3da08d7701cdaf2086391393ce708ceba7e6
khalilcherni <kcherni411@gmail.com>
2024-02-13 16:32:52 +0100

    36

--- Frontend/my-app/app/sign-up/page.jsx
+++ Frontend/my-app/app/sign-up/page.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import {GoogleProvider } from '@/app/firebase/config'
+// import { useSignInWithPhoneNumber } from 'react-firebase-hooks/auth';
 import {auth} from '@/app/firebase/config'
 import { useRouter } from 'next/navigation';
 
@@ -12,6 +13,7 @@ const SignUp = () => {
   const [password, setPassword] = useState('');
   const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
   const [signInWithGoogle] = useSignInWithGoogle(auth);
+  // const [signInWithPhone] = useSignInWithPhoneNumber(auth);
   const router = useRouter();
 
   const handleSignUp = async () => {
@@ -39,6 +41,15 @@ const SignUp = () => {
       console.error(e);
     }
   };
+  // const handlePhoneSignUp = async () => {
+  //   try {
+  //     const confirmationResult = await signInWithPhone(phone);
+  //     // Handle confirmation result, e.g., show UI for entering verification code
+  //   } catch (error) {
+  //     // Handle error, e.g., display error message to the user
+  //     console.error("Error signing in with phone number:", error);
+  //   }
+  // };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
@@ -70,6 +81,7 @@ const SignUp = () => {
         >
           <img src="https://img.icons8.com/?size=80&id=6QtoKjRma1Cq&format=png" alt="" />
         </button>
+      
         <button onClick={() => router.push('/sign-in')} className="signin-link">Sign In</button>
       </div>
     </div>
