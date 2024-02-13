464d825f49432317f388078df429e38ad82037ef
khalilcherni <kcherni411@gmail.com>
2024-02-13 22:03:43 +0100

    38

--- Frontend/my-app/next.config.mjs
+++ Frontend/my-app/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// next.config.js
 
+export const metadata = {
+    title: "Your Next.js App",
+    description: "Description of your Next.js app",
+  };
+  
 export default nextConfig;
