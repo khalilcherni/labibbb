0c29656e4e7ea9b7eb9bc96342daf3a9a59d23c1
khalilcherni <kcherni411@gmail.com>
2024-02-14 13:48:48 +0100

    56

--- Frontend/my-app/app/Navbar/page.jsx
+++ Frontend/my-app/app/Navbar/page.jsx
@@ -4,31 +4,15 @@ import Link from 'next/link';
 import { AppBar, Box, Toolbar, IconButton, Typography, Menu, MenuItem, Avatar, Button, Tooltip } from '@mui/material';
 import { Menu as MenuIcon, Adb as AdbIcon } from '@mui/icons-material';
 import './Navbar.css'
-const pages = ['Products', 'Pricing', 'Blog'];
+
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
+ 
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
-    <AppBar className='kh' position="static">
+    <AppBar className='kh' >
       <Toolbar disableGutters>
         <AdbIcon />
         <Typography
@@ -59,28 +43,16 @@ function ResponsiveAppBar() {
 
         <Box >
           <Tooltip title="Open settings">
-            <IconButton onClick={handleOpenUserMenu} >
+            <IconButton >
               <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
             </IconButton>
           </Tooltip>
           <Menu
-            sx={{ mt: '45px' }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
+           
+         
           >
             {settings.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+              <MenuItem key={setting} >
                 <Typography textAlign="center">{setting}</Typography>
               </MenuItem>
             ))}
